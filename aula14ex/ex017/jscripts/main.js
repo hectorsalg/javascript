9a9712d5b1013454a20b738e77680e4a7b43e0ea
Hector Salgueiros <99827871+hectorsalg@users.noreply.github.com>
2022-10-15 20:05:10 -0300

    atualizei ex017

--- aula14ex/ex017/jscripts/main.js
+++ aula14ex/ex017/jscripts/main.js
@@ -1,14 +1,17 @@
-function calcular() {
-    var num = Number(document.getElementById('inum').value)
-    var msg = document.getElementById('imsg')
-    var count = 1
-    msg.value = ''
-    if (num == '') {
+function tabuada() {
+    let num = document.getElementById('inum')
+    let msg = document.getElementById('itab')
+    if (num.value.length == 0) {
         alert('Por favor, digite um número.')
     } else {
-        msg.cols = '15'
+        let n = Number(num.value)
+        let count = 1
+        msg.innerHTML = ''
         for (count; count <= 10; count++) {
-            msg.value += `${num} x ${count} = ${num * count}\n`
+            let item = document.createElement('option')
+            item.text = `${n} x ${count} = ${n * count}\n`
+            item.value = `tab${count}`
+            msg.appendChild(item)
         }
     }
 }
