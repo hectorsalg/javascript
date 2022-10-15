3b999d150c9abbef693843bb95906b9c380addd9
Hector Salgueiros <99827871+hectorsalg@users.noreply.github.com>
2022-10-15 02:03:04 -0300

    fiz exercício da aula 13

---
+++ aula13/ambiente.js
@@ -0,0 +1,24 @@
+var count = 1
+do {
+    console.log(`Passo ${count}`)
+    count++
+} while (count >= 6)
+
+
+/*
+var count = 1
+while (count <= 6) {
+    console.log(`Passo ${count}`)
+    count++
+}
+*/
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
